2fe4d114151692375941d4868e56a1eca7312323
Eder Canada <canada.eder@gmail.com>
2022-03-27 18:50:54 -0700

    remove from favorites

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { getMovies } from './services/getMovies';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
+import Like from './components/Like';
+import RemoveFavorite from './components/RemoveFavorites';
 import './App.css';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState('star wars');
+  const [like, setLike] = useState([]);
 
   const getMoviesRequest = async () => {
     const API_KEY = process.env.REACT_APP_API_KEY;
@@ -23,6 +26,22 @@ const App = () => {
     getMoviesRequest(searchTerm);
   }, [searchTerm]);
 
+  const addFavoriteMovie = (movie) => {
+    const newFavoriteMovieList = [...like, movie];
+    setLike(newFavoriteMovieList);
+  };
+
+  const removeFavoriteMovie = (movie) => {
+    console.log(movie, 'movie filter');
+    const newFavoriteMovieList = [...like, movie];
+
+    const filteredMovies = newFavoriteMovieList.filter(
+      (movieFromList) => movieFromList.imdbID != movie.imdbID
+    );
+
+    setLike(filteredMovies);
+  };
+
   return (
     <div className="container-fluid movie-app">
       <div className="row d-flex align-items-center mt-4 mb-4">
@@ -30,7 +49,21 @@ const App = () => {
         <SearchBox searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
       <div className="row ">
-        <MovieList allMovies={movies} />
+        <MovieList
+          allMovies={movies}
+          Like={Like}
+          handleMovieLike={addFavoriteMovie}
+        />
+      </div>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Favorites" />
+      </div>
+      <div className="row ">
+        <MovieList
+          allMovies={like}
+          Like={RemoveFavorite}
+          handleMovieLike={removeFavoriteMovie}
+        />
       </div>
     </div>
   );
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
-const MovieList = ({ allMovies }) => {
+const MovieList = ({ allMovies, Like, handleMovieLike }) => {
   return (
     <>
       {allMovies.map((movie, index) => {
         return (
-          <div className="d-flex justify-content-start m-3" key={index}>
+          <div
+            className="d-flex justify-content-start m-3 image-container"
+            key={index}
+          >
             <img src={movie.Poster} alt={movie.Title} />
+            <div
+              onClick={() => handleMovieLike(movie)}
+              className="overlay d-flex align-items-center justify-content-center"
+            >
+              <Like />
+            </div>
           </div>
         );
       })}
